3c32c8f0cf7cd4b62e505efe25196580d9fe2979
AHMETCAN_EFE <72448523+ahmetcanefe@users.noreply.github.com>
2022-07-16 12:42:44 +0300

    shopapp

---
+++ shopapp.data/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using shopapp.entity;
+
+namespace shopapp.data.Concrete.EfCore
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ShopApp;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategory>()
+                .HasKey(c => new { c.CategoryId, c.ProductId });
+        }
+
+
+    }
+}
